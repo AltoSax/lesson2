e2322ed6da73a091ddcec7e6d8539856b6a4834d
AltoSax <AltoSax00@bk.ru>
2016-07-28 15:47:20 +0400

    Create Project Structure

---
+++ src/main/java/programm/Programm.java
@@ -0,0 +1,11 @@
+package programm;
+
+/**
+ * Created by Savva Avdysho on 28.07.2016.
+ */
+
+public class Programm {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
